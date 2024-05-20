d8049e17c9d746ed8a36c82cca612445f614b02e
Tomas26071988 <artem_1988_artem1988@icloud.com>
2024-05-20 23:39:48 +0300

    Add files via upload

---
+++ Homework6 Dict.py
@@ -0,0 +1,7 @@
+my_dict = {'House': 'Дом', 'Green': 'Зеленый'}
+print(my_dict)
+print(my_dict.values())
+my_dict['House'] = 'Квартира'   # изменил значение для ключа
+print(my_dict)
+my_dict.update({'Room': 'Комната', 'Palace': 'Замок'})   # Добавление двух новых
+print(my_dict)
---
+++ homework6 list.py
@@ -0,0 +1,7 @@
+my_list = ['orange', 'apple', 'tomato', 'avocado']
+print(my_list)
+print('first element:', my_list[0])    # вывел первый элемент
+print('Last element:', my_list[3])     # вывел последний
+print(my_list[2:5])       # с третьего до пятого
+my_list[2] = 'grape'    # Изменил томато на виноград
+print(my_list)
